eac67e2cfd5d856168692b099ce847541d978c1f
Ben Kamphaus <ben.kamphaus@gmail.com>
2024-01-01 13:54:03 -0700

    clean up docs gen path, dispatch dir or datomic-uri

--- src/clj/org/parkerici/alzabo/core.clj
+++ src/clj/org/parkerici/alzabo/core.clj
@@ -1,5 +1,6 @@
 (ns org.parkerici.alzabo.core
   (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
             [org.parkerici.alzabo.unify :as unify]
             [org.parkerici.alzabo.service :refer [serve-static]]
             [org.parkerici.alzabo.config :as config]
@@ -10,7 +11,7 @@
   (:import (java.awt Desktop)))
 
 
-(def SCHEMA-DIR
+(defn schema-dir []
   (or (System/getenv "UNIFY_SCHEMA_DIRECTORY")
       "../unify/test/resources/systems/candel/template-dataset/schema"))
 
@@ -19,30 +20,20 @@
   (.browse (Desktop/getDesktop)
            (.toURI (io/file file))))
 
-(defn- schema
-  [schema-dir]
-  (let [schema (unify/parse-schema-files schema-dir)]
+(defn- dir->schema
+  []
+  (let [schema (unify/parse-schema-files (schema-dir))]
+    (config/set! :version (:version schema))
+    schema))
+
+(defn- db->schema
+  []
+  (let [schema (unify/db->unify-schema)]
     (config/set! :version (:version schema))
     schema))
 
 ;;; New config-file machinery
 
-(defmulti do-command (fn [command args] (keyword command)))
-
-(defmethod do-command :browser
-  [_ _]
-  (schema SCHEMA-DIR)
-  (browse-file (config/output-path "index.html")))
-
-(defmethod do-command :server
-  [_ _]
-  (schema SCHEMA-DIR)
-  (serve-static "/public" {:dev false}))
-
-(defmethod do-command :dev-server
-  [_ _]
-  (schema SCHEMA-DIR)
-  (serve-static "/public" {:dev true}))
 
 (defn write-alzabo
   [schema]
@@ -50,24 +41,49 @@
 
 (defn read-alzabo
   [schema-file]
-  (read-string (slurp schema-file)))
+  (edn/read-string (slurp schema-file)))
 
-(defmethod do-command :documentation
-  [_ _] 
-  (let [unify? (= (config/config :source) :unify)
-        schema (if unify?
-                 (schema SCHEMA-DIR)
-                 (read-alzabo (config/config :source)))]
+(defn schema->html
+  []
+  (let [schema-source (config/config :source)
+        unify? (= :unify schema-source)
+        unify-db? (= :unify-db schema-source)
+        schema (cond
+                 unify? (dir->schema)
+                 unify-db? (db->schema)
+                 :else (read-alzabo schema-source))]
     ;; TODO: (BK 1/1/2024) this writes an alzabo schema, but afaict this is only ever
     ;;       checked by tests and not used in cljs, etc? cljs path writes
     ;;       its alzabo schema file separately to resources.
-    (when (= (config/config :source) :unify)
+    (when (#{:unify :unify-db} schema-source)
       (write-alzabo schema))
     (html/schema->html schema)))
 
+(defmulti do-command (fn [command args] (keyword command)))
+
+(defmethod do-command :documentation
+  [_ _]
+  (schema->html))
+
+(defmethod do-command :browser
+  [_ _]
+  (schema->html)
+  (browse-file (config/output-path "index.html")))
+
+(defmethod do-command :server
+  [_ _]
+  (schema->html)
+  (serve-static "/public" {:dev false}))
+
+(defmethod do-command :dev-server
+  [_ _]
+  (schema->html)
+  (serve-static "/public" {:dev true}))
+
 (defmethod do-command :datomic
   [_ _]
-  (let [schema (schema SCHEMA-DIR)]
+  (let [schema-file (config/config :source)
+        schema (read-alzabo schema-file)]
     (write-alzabo schema)
     (output/write-schema (datomic/datomic-schema schema)
                          (config/output-path "datomic-schema.edn"))
@@ -90,6 +106,8 @@
 (comment
   (main* "resources/candel-config.edn" :documentation)
   (main* "test/resources/rawsugar-config.edn" :documentation)
+  (main* "resources/unify-db-config.edn" :documentation)
   ;; this puts schema at: http://localhost:8899/schema/1.3.1/index.html
   (main* "resources/candel-config.edn" :server)
+  (main* "resources/unify-db-config.edn" :server)
   (main* "resources/candel-config.edn" :dev-server))
