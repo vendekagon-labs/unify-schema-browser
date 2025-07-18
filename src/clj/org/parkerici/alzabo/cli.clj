dc4460dea4d490fe2e5d7d8e0f60dd8b61b0e2ca
Ben Kamphaus <ben.kamphaus@gmail.com>
2025-07-18 07:21:08 -0600

    simplify cli, wrap mem db for schema dir render

--- src/clj/org/parkerici/alzabo/cli.clj
+++ src/clj/org/parkerici/alzabo/cli.clj
@@ -4,6 +4,7 @@
             [datomic.api :as d]
             [org.parkerici.alzabo.config :as config]
             [org.parkerici.alzabo.unify :as unify]
+            [org.parkerici.alzabo.unify.schema-directory :as unify-schema]
             [org.parkerici.alzabo.unify.query :as query]
             [org.parkerici.alzabo.html :as html]))
 
@@ -41,65 +42,20 @@
                  " e.g. with: " \newline)
         (println "open" result-path)))))
 
-(defn usage [options-summary]
-  (->> ["This is my program. There are many like it, but this one is mine."
-        ""
-        "Usage: program-name [options] action"
-        ""
-        "Options:"
-        options-summary
-        ""
-        "Actions:"
-        "  start    Start a new server"
-        "  stop     Stop an existing server"
-        "  status   Print a server's status"
-        ""
-        "Please refer to the manual page for more information."]
-       (str/join \newline)))
+(defn render-from-schema-dir [schema-dir output-dir]
+  (let [datomic-uri "datomic:mem://unify-schema-browser-temp"
+        _ (d/create-database datomic-uri)]
+    (unify-schema/apply-schema schema-dir datomic-uri)
+    (render-from-db-uri datomic-uri output-dir)))
 
-(defn error-msg [errors]
-  (str "The following errors occurred while parsing your command:\n\n"
-       (str/join \newline errors)))
-
-
-(def cli-options
-  ;; An option with an argument
-  [[nil "--schema-directory SCHEMA-DIRECTORY" "Path to directory with a Unify schema"
-    :id :schema-dir]
-   ;; A non-idempotent option (:default is applied first)
-   [nil "--datomic-uri DATOMIC-URI" "URI for a Datomic database with a Unify schema"
-    :id :datomic-uri]
-   ;; :assoc-fn (fn [m k _] (update-in m [k] inc))
-   ;; A boolean option defaulting to nil
-   ["-o" "--output-directory" "Directory where html, etc for schema browser will be written."]
-   ["-h" "--help"]])
-
-(defn validate-args
-  "Validate command line arguments. Either return a map indicating the program
-  should exit (with an error message, and optional ok status), or a map
-  indicating the action the program should take and the options provided."
-  [args]
-  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
-    (cond
-      (:help options) ; help => exit OK with usage summary
-      {:exit-message (usage summary) :ok? true}
-      errors ; errors => exit with description of errors
-      {:exit-message (error-msg errors)}
-      ;; custom validation on arguments
-      (and (= 1 (count arguments))
-           (#{"start" "stop" "status"} (first arguments)))
-      {:action (first arguments) :options options}
-      :else ; failed custom validation => exit with usage summary
-      {:exit-message (usage summary)})))
-
-(defn exit [status msg]
-  (println msg)
-  #_(System/exit status))
-
-(defn -main [& args]
-  (let [{:keys [options exit-message ok?]} (validate-args args)]
-    (if exit-message
-      (exit (if ok? 0 1) exit-message))))
+(defn -main [[data-src]]
+  ;; check datomic: prefix, if so do that
+  ;; check directory exists or can be created, if so do that
+  ;; otherwise error
+  (if (str/starts-with? data-src "datomic:")
+    :yes
+    :no))
 
 (comment
-  (render-from-db-uri "datomic:ddb://us-east-1/data-commons-dev-1/h37001" "test-render"))
+  (render-from-db-uri "datomic:ddb://us-east-1/data-commons-dev-1/h37001" "test-render")
+  (render-from-schema-dir "/Users/benjaminkamphaus/code/unify/test/resources/systems/candel/template-dataset/schema" "test-render"))
